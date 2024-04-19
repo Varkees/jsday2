13fcaedd951e3382ada1b6cf367b1fdf95ef6f6f
email <jackvarkees@gmail.con>
2024-04-20 00:46:21 +0530

    jack

---
+++ script.js
@@ -0,0 +1,63 @@
+// var jack = 0;
+
+// console.log(jack);
+
+// let jack = 9;
+
+// console.log(jack);
+
+// const jack = 'varkees'
+
+// console.log(jack);
+// ===========================/
+
+// var jack = 24;
+
+// {
+//     var malar =45;
+//     console.log(jack);
+// }
+// console.log(malar);
+
+// ===========================
+
+// let jack = 'varkees';
+//  let jack='jackvarkeees';
+// console.log(jack);
+
+// ===================================
+
+// let product  ='laptop';
+// let prize = 15000;
+// let Tax = 4500;
+// console.log(product);
+// console.log(prize+Tax);
+
+
+// =================================================
+
+
+// let bike = hero honda;
+
+// console.log(bike);
+
+
+// ===========================================
+
+
+
+// let FruitName = 'pineApple';
+
+// let count = 40;
+ 
+// let price = 25;
+
+
+// console.log(FruitName);
+
+// let total = (count*price);
+
+// console.log(total);
+
+
+// ====================================
